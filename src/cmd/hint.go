367fbaef2b082d0b76cdf0a7afa015f55f5d77c5
Maurício Antunes <mauricio.abreua@gmail.com>
2022-10-29 23:44:11 -0300

    feat: add hint command

---
+++ src/cmd/hint.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/mauricioabreu/golings/src/exercises"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(cmdHint)
+}
+
+var cmdHint = &cobra.Command{
+	Use:   "hint",
+	Short: "Get a hint for an exercise",
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Run: func(cmd *cobra.Command, args []string) {
+		exercise, err := exercises.Find(args[0])
+		if err != nil {
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+		color.Yellow(exercise.Hint)
+	},
+}
