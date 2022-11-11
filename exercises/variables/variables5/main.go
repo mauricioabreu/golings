bd100bb81e0a98221d3b20c50addfc5ca4a82a9e
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-11 20:37:45 -0300

    feat: one more variables exercise

---
+++ exercises/variables/variables5/main.go
@@ -0,0 +1,13 @@
+// variables5
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+const Pi
+
+func main() {
+	fmt.Println(Pi)
+}
---
+++ exercises/variables/variables6/main.go
@@ -0,0 +1,16 @@
+// variables6
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+const x = 10
+
+func main() {
+	fmt.Println(x)
+
+	x = x + 1
+	fmt.Println(x)
+}
