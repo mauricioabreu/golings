f9682e63d94b4f27db97ce7f31e5cabeb22664c9
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-11 20:08:28 -0300

    feat: one more variables exercise

---
+++ exercises/variables/variables4/main.go
@@ -0,0 +1,19 @@
+// variables4
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	x := 10 // Don't change this line
+	fmt.Printf("x has the value %d", x)
+
+	if true {
+		x = "foo"
+		fmt.Println(x)
+	}
+
+	fmt.Println(x)
+}
