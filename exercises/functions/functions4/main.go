0ee76764b52e325a5bc87b76df8f9e79568916b3
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-12 20:21:49 -0300

    feat: add one more functions exercise

---
+++ exercises/functions/functions4/main.go
@@ -0,0 +1,15 @@
+// functions4
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 2 is: ", add_numbers(1, 2)) // don't change this line
+}
+
+func add_numbers(a int, b int) {
+	return a + b
+}
