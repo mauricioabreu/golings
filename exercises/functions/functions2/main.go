568626a2db9933c2864573f081f0d44160aec14a
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-12 06:37:31 -0300

    feat: add one more functions exercise

---
+++ exercises/functions/functions2/main.go
@@ -0,0 +1,17 @@
+// functions2
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	call_me(10)
+}
+
+func call_me(num) {
+	for n := 0; n <= num; n++ {
+		fmt.Printf("Num is %d\n", n)
+	}
+}
