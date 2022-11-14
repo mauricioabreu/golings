fd4caed165737f6ffbaef02eb85139f1feaa765d
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-14 20:47:12 -0300

    feat: add one more if exercise

---
+++ exercises/if/if2/main_test.go
@@ -0,0 +1,36 @@
+// if1
+// Make me compile!
+
+// I AM NOT DONE
+package main_test
+
+import "testing"
+
+func fooIfFizz(fizzish string) string {
+	if fizzish == "fizz" {
+		return "foo"
+	} else {
+		return "complete me"
+	}
+}
+
+func TestFooForFizz(t *testing.T) {
+	result := fooIfFizz("fizz")
+	if result != "foo" {
+		t.Errorf("should be 'foo' but got %s", result)
+	}
+}
+
+func TestBarForFuzz(t *testing.T) {
+	result := fooIfFizz("fuzz")
+	if result != "bar" {
+		t.Errorf("should be 'bar' but got %s", result)
+	}
+}
+
+func TestDefaultForBazz(t *testing.T) {
+	result := fooIfFizz("random stuff")
+	if result != "baz" {
+		t.Errorf("should be 'baz' but got %s", result)
+	}
+}
