cb8e07a53feefc1ba376cd679b96eeb587a87138
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-17 08:48:01 -0300

    feat: add one more switch exercise

---
+++ exercises/switch/switch3/main_test.go
@@ -0,0 +1,35 @@
+// switch3
+// Make me compile!
+
+// I AM NOT DONE
+package main_test
+
+import "testing"
+
+func weekDay(day int) string {
+	// Return the day of the week based on the
+	// integer. Use a switch case to satisfy all test cases below
+	return "Sunday"
+}
+
+func TestWeekDay(t *testing.T) {
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{input: 0, want: "Sunday"},
+		{input: 1, want: "Monday"},
+		{input: 2, want: "Tuesday"},
+		{input: 3, want: "Wednesday"},
+		{input: 4, want: "Thursday"},
+		{input: 5, want: "Friday"},
+		{input: 6, want: "Saturday"},
+	}
+
+	for _, tc := range tests {
+		day := weekDay(tc.input)
+		if day != tc.want {
+			t.Errorf("expected %s but got %s", tc.want, day)
+		}
+	}
+}
