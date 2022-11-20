eaa5a7968242e50a4bbc86a180942fd76e850bf6
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-20 10:51:42 -0300

    feat: add one moe primitive types exercise

---
+++ exercises/primitive_types/primitive_types5/main.go
@@ -0,0 +1,19 @@
+// primitive_types5
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	var n1 integer = 101
+	if n1 > 100 {
+		fmt.Println("It is a big number")
+	} else {
+		fmt.Println("Not a big number at all")
+	}
+
+	var n2 float = 0.99
+	fmt.Println(n2)
+}
