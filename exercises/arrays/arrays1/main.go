f8c1f63f66840fc96b9b2eed93b99215a65cc15c
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-20 07:53:41 -0300

    feat: add first array exercise

---
+++ exercises/arrays/arrays1/main.go
@@ -0,0 +1,18 @@
+// arrays1
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	var colors [3]string
+
+	colors[0] = "red"
+	colors[1] = "green"
+	colors[2] = "blue"
+
+	fmt.Printf("First color is %s:\n", colors[])
+	fmt.Printf("Last color is %s:\n", colors[])
+}
