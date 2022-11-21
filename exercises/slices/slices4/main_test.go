525d58dc41bf3d92427b53892a5b676c9ce00926
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-21 18:45:15 -0300

    feat: add test exercise for slices

---
+++ exercises/slices/slices4/main_test.go
@@ -0,0 +1,39 @@
+// slices4
+// Make me compile!
+
+// I AM NOT DONE
+package main_test
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetOnlyFirstName(t *testing.T) {
+	names := []string{"John", "Maria", "Carl", "Peter"}
+	firstName := names[3]
+
+	if firstName != "John" {
+		t.Errorf("firstName value must be John")
+	}
+}
+
+func TestGetFirstTwoNames(t *testing.T) {
+	names := []string{"John", "Maria", "Carl", "Peter"}
+	firstTwoNames := names[5:10]
+	expectedFirstTwoNames := []string{"John", "Maria"}
+
+	if !reflect.DeepEqual(firstTwoNames, expectedFirstTwoNames) {
+		t.Errorf("firstTwoNames should be %v, but got %v", expectedFirstTwoNames, firstTwoNames)
+	}
+}
+
+func TestGetLastTwoNames(t *testing.T) {
+	names := []string{"John", "Maria", "Carl", "Peter"}
+	lastTwoNames := names[5:10]
+	expectedLastTwoNames := []string{"Carl", "Peter"}
+
+	if !reflect.DeepEqual(lastTwoNames, expectedLastTwoNames) {
+		t.Errorf("firstTwoNames should be %v, but got %v", expectedLastTwoNames, lastTwoNames)
+	}
+}
