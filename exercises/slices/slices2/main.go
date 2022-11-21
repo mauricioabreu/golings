2bde005f6a8dc5e5326f04d1d6fb8598ddd845ee
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-21 10:06:25 -0300

    feat: add one more slice exercise

---
+++ exercises/slices/slices2/main.go
@@ -0,0 +1,13 @@
+// slices2
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	names := [4]string{"John", "Maria", "Carl", "Peter"}
+	lastTwoNames := names[] // after figuring out the answer, try with other low/high bounds
+	fmt.Println(lastTwoNames)
+}
