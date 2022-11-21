7f67d60b889c80cff69028ee44b9717129ecf938
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-21 08:23:04 -0300

    feat: add first slice exercise

---
+++ exercises/slices/slices1/main.go
@@ -0,0 +1,13 @@
+// slices1
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	a := make(, 3, 10) // play with length and capacity
+	fmt.Println("length of 'a':", len(a))
+	fmt.Println("capacity of 'a':", cap(a))
+}
