62683bb3ea051c798d8ec7723c1b10870cae963a
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-21 18:17:00 -0300

    feat: add one more slice exercise

---
+++ exercises/slices/slices3/main.go
@@ -0,0 +1,13 @@
+// slices3
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{"John", "Maria", "Carl", "Peter"}
+	names = append()
+	fmt.Println(names)
+}
