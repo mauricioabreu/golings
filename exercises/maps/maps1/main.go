df05d6846a856ded638406bae0b863ade6413626
Maurício Antunes <mauricio.abreua@gmail.com>
2022-11-28 11:24:17 -0300

    feat: new map exercise

---
+++ exercises/maps/maps1/main.go
@@ -0,0 +1,16 @@
+// maps1
+// Make me compile!
+//
+// I AM NOT DONE
+//
+package main
+
+func main() {
+	// Map with people names and their ages
+	m := make(map)
+
+	m["John"] = 30
+	m["Ana"] = 21
+
+	fmt.Printf("John is %d and Ana is %d", m[], m[])
+}
