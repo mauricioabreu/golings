49cdff615ce2e50a4d4c1359137f816de6e4b853
Maurício Antunes <mauricio.abreua@gmail.com>
2022-12-05 18:22:21 -0300

    feat: add range exercises

---
+++ exercises/range/range1/main.go
@@ -0,0 +1,15 @@
+// range1
+// Make me compile!
+//
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	evenNumbers := []int{2, 4, 6, 8, 10}
+
+	for _, v := {
+		fmt.Printf("%d is even\n", v)
+	}
+}
---
+++ exercises/range/range2/main.go
@@ -0,0 +1,18 @@
+// range2
+// Make me compile!
+//
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	phoneBook := map[string]string{
+		"Ana":  "+01 101 102",
+		"John": "+01 333 666",
+	}
+
+	for := range {
+		fmt.Printf("%s has the %s phone\n", name, phone)
+	}
+}
