6f8aff75f38c822ee9064d93081b582313534eb7
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2023-11-03 20:33:27 -0400

    fix typos

--- golings/cmd/watch.go
+++ golings/cmd/watch.go
@@ -50,7 +50,7 @@ func WatchCmd(infoFile string) *cobra.Command {
 					color.Green("Bye by golings o/")
 					os.Exit(0)
 				default:
-					color.Yellow("only list or hint command are avaliable")
+					color.Yellow("only list or hint commands are available")
 				}
 			}
 		},
